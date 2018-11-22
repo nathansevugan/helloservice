7ccaca41788a26ef6054520ab4185e98d9515903
Sevuganchetty, Ranganathan <ranganathan.sevuganchetty@sabre.com>
2018-11-22 12:32:18 -0600

    simple hello service

---
+++ helloservice.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello from service!"
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
