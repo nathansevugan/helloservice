01d1a81121ddeb2342cbf664f98064919e073604
Sevuganchetty, Ranganathan <ranganathan.sevuganchetty@sabre.com>
2018-11-22 17:43:58 -0600

    simple hello service

--- helloservice.py
+++ helloservice.py
@@ -1,46 +1,27 @@
-#!/usr/bin/env python
-"""
-Very simple HTTP server in python.
-Usage::
-    ./dummy-web-server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-"""
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+import os
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
 
-    def do_HEAD(self):
-        self._set_headers()
 
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
 
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    httpd.serve_forever()
+def run():
+  print('http server is starting...')
 
-if __name__ == "__main__":
-    from sys import argv
+  #ip and port of server
+  #by default http server port is 80
+  server_address = ('127.0.0.1', 8080)
+  httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+  print('http server is running...')
+  httpd.serve_forever()
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+if __name__ == '__main__':
+  run()
