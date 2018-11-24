d6af95d5d27b0ad556b1f4300ed8cb9fce19bc62
Sevuganchetty, Ranganathan <ranganathan.sevuganchetty@sabre.com>
2018-11-23 18:56:08 -0600

    changed the greetings message

--- helloservice.py
+++ helloservice.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, it is a wonderful day today!"
+    return "Hello, it is a wonderful day today - another greetings bud!"
 
 
 if __name__ == '__main__':
