ab0273f93ab1c5080471811a82bb38ee5edce378
Sevuganchetty, Ranganathan <ranganathan.sevuganchetty@sabre.com>
2018-11-25 18:33:21 -0600

    added config map

--- helloservice.py
+++ helloservice.py
@@ -1,11 +1,20 @@
+import sys
+
 from flask import Flask
 app = Flask(__name__)
+greeting = 'hello'
+message = 'world'
 
 
 @app.route("/")
 def hello():
-    return "Hello, it is a wonderful day today - trying to run tests!"
+    return greeting + " " + message
 
 
 if __name__ == '__main__':
+    if (len(sys.argv) == 3):
+        print "overriding default greetings.."
+        greeting = sys.argv[1]
+        message = sys.argv[2]
+
     app.run(debug=False, host='0.0.0.0')
